f399a97d51b0b523cebb386d50d97237f619a373
rtvc <rtvc@6f5d9dc4-7504-4b2d-819e-181cc167b626>
2009-04-26 19:23:35 +0000

    First version of VideoMixingBase

---
+++ trunk/videoprocessing/Source/RtvcLib/DirectShow/VideoMixingBase.cpp
@@ -0,0 +1,327 @@
+/** @file
+
+MODULE                : VideoMixingBase
+
+FILE NAME            : VideoMixingBase.cpp
+
+DESCRIPTION            :
+                     
+LICENSE: Software License Agreement (BSD License)
+
+Copyright (c) 2008, Meraka Institute
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+* Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+* Neither the name of the Meraka Institute nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+===========================================================================
+*/
+#include "stdafx.h"
+
+// Meraka includes
+#include "VideoMixingBase.h"
+
+#include <DirectShow/CommonDefs.h>
+
+VideoMixingBase::VideoMixingBase(TCHAR *pObjectName, LPUNKNOWN lpUnk, CLSID clsid)
+:CMultiIOBaseFilter(pObjectName, lpUnk, clsid),
+m_nOutputWidth(0),
+m_nOutputHeight(0),
+m_nOutputSize(0),
+m_tStartTime1(0),
+m_tStartTime2(0),
+m_tStopTime1(0),
+m_tStopTime2(0)
+{
+	//Hack virtual method of subclass can't be called from base class contructor, hence must be called in child contructor
+	Initialise();
+}
+
+VideoMixingBase::~VideoMixingBase()
+{}
+
+HRESULT VideoMixingBase::Receive( IMediaSample *pSample, int nIndex )
+{
+	CAutoLock lck(&m_csReceive);
+	ASSERT(nIndex >= 0);
+	ASSERT (nIndex < m_vInputPins.size());
+	ASSERT(pSample);
+	ASSERT (m_vOutputPins[0] != NULL);
+	
+	if (nIndex == 0)
+	{
+		return ReceiveFirstSample(pSample);
+	}
+	else
+	{
+		return ReceiveSecondSample(pSample);
+	}
+}
+
+HRESULT VideoMixingBase::DecideBufferSize( IMemAllocator* pAlloc, ALLOCATOR_PROPERTIES* pRequestProperties, int m_nIndex )
+{
+	// Get media type of the output pin
+	AM_MEDIA_TYPE mt;
+	HRESULT hr = m_vOutputPins[m_nIndex]->ConnectionMediaType(&mt);
+	if (FAILED(hr))
+	{
+		return hr;
+	} 
+	BITMAPINFOHEADER *pbmi = HEADER(mt.pbFormat);
+
+	//For RGB formats, hence we can just use the DIBSIZE of the bitmapinfoheader
+	pRequestProperties->cbBuffer = DIBSIZE(*pbmi);
+
+	if (pRequestProperties->cbAlign == 0)
+	{
+		pRequestProperties->cbAlign = 1;
+	}
+	if (pRequestProperties->cBuffers == 0)
+	{
+		pRequestProperties->cBuffers = 1;
+	}
+	// Release the format block.
+	FreeMediaType(mt);
+
+	// Set allocator properties.
+	ALLOCATOR_PROPERTIES Actual;
+	hr = pAlloc->SetProperties(pRequestProperties, &Actual);
+	if (FAILED(hr)) 
+	{
+		return hr;
+	}
+	// Even when it succeeds, check the actual result.
+	if (pRequestProperties->cbBuffer > Actual.cbBuffer) 
+	{
+		return E_FAIL;
+	}
+	return S_OK;
+
+}
+
+HRESULT VideoMixingBase::GetMediaType( int iPosition, CMediaType* pMediaType, int nOutputPinIndex )
+{
+	if (iPosition < 0)
+	{
+		return E_INVALIDARG;
+	}
+	else if (iPosition == 0)
+	{
+
+		CMultiIOInputPin* pConnectedPin = NULL;
+		if (m_vInputPins[0]->IsConnected())
+		{
+			pConnectedPin = m_vInputPins[0];
+		}
+		else
+		{
+			if (m_vInputPins[1]->IsConnected())
+			{
+				pConnectedPin = m_vInputPins[1];
+			}
+			else
+			{
+				// Error: no connected input pins
+				return E_FAIL;
+			}
+		}
+
+		HRESULT hr = pConnectedPin->ConnectionMediaType(pMediaType);
+		if (FAILED(hr))
+		{
+			return hr;
+		}
+
+		VIDEOINFOHEADER* pVih1 = (VIDEOINFOHEADER*)pMediaType->pbFormat;
+		BITMAPINFOHEADER* bmh1 = &(pVih1->bmiHeader);
+
+		// Adjust media parameters
+		bmh1->biWidth = m_nOutputWidth;
+		bmh1->biHeight = m_nOutputHeight;
+
+		bmh1->biSizeImage = m_nOutputSize;
+		//Set sample size
+		pMediaType->SetSampleSize(m_nOutputSize);
+
+		// Set source rect
+		pVih1->rcSource.left = 0;
+		pVih1->rcSource.top = 0;
+		pVih1->rcSource.right = m_nOutputWidth;
+		pVih1->rcSource.bottom = m_nOutputHeight;
+
+		// Set target rect
+		pVih1->rcTarget.left = 0;
+		pVih1->rcTarget.top = 0;
+		pVih1->rcTarget.right = m_nOutputWidth;
+		pVih1->rcTarget.bottom = m_nOutputHeight;
+		return S_OK;
+	}
+	return VFW_S_NO_MORE_ITEMS;
+}
+
+HRESULT VideoMixingBase::SetMediaType( PIN_DIRECTION direction, const CMediaType *pmt, int nIndex )
+{
+	// Check if our base class accepts the connection
+	HRESULT hr = CMultiIOBaseFilter::SetMediaType(direction, pmt, nIndex);
+	if (FAILED(hr))
+	{
+		return hr;
+	}
+	//TODO: Check if the sizes match: The images must correlate in dimension!!!!!!!!!!!
+	if (direction == PINDIR_INPUT)
+	{
+		// Copy the video info header
+		VIDEOINFOHEADER* pVih = (VIDEOINFOHEADER*) pmt->pbFormat;
+		BITMAPINFOHEADER bmih = pVih->bmiHeader;
+		CopyMemory(&m_VideoInHeader[nIndex], pVih, sizeof(VIDEOINFOHEADER));
+		
+		// Reject connection if output is already connected
+		if (m_vOutputPins[0]->IsConnected())
+		{
+			return E_FAIL;
+		}
+
+		hr = CreateVideoMixer(pmt, nIndex);
+		if (FAILED(hr))
+		{
+			return hr;
+		}
+
+		// Get first input dimensions
+		AM_MEDIA_TYPE mediaType1;
+		BITMAPINFOHEADER* pBmih1 = NULL;
+		if (m_vInputPins[0]->IsConnected())
+		{
+			hr = m_vInputPins[0]->ConnectionMediaType(&mediaType1);
+			if (FAILED(hr))
+			{
+				return hr;
+			}
+			VIDEOINFOHEADER* pVih = (VIDEOINFOHEADER*) mediaType1.pbFormat;
+			pBmih1 = &pVih->bmiHeader;
+		}
+
+		// Get second input dimensions
+		BITMAPINFOHEADER* pBmih2 = NULL;
+		AM_MEDIA_TYPE mediaType2;
+		if (m_vInputPins[1]->IsConnected())
+		{
+			hr = m_vInputPins[1]->ConnectionMediaType(&mediaType2);
+			if (FAILED(hr))
+			{
+				return hr;
+			}
+			VIDEOINFOHEADER* pVih = (VIDEOINFOHEADER*) mediaType2.pbFormat;
+			pBmih2 = &pVih->bmiHeader;
+		}
+
+		// Leave the output dimensions up to the sub class
+		HRESULT hr = SetOutputDimensions(pBmih1, pBmih2);
+
+		// Free format blocks
+		if (pBmih1)
+			FreeMediaType(mediaType1);
+		if (pBmih2)
+			FreeMediaType(mediaType2);
+		return hr;
+	}
+	return S_OK;
+}
+
+STDMETHODIMP VideoMixingBase::Stop()
+{
+	CAutoLock lck1(&m_csFilter);
+	if (m_State == State_Stopped) {
+		return NOERROR;
+	}
+	// If the pins are not connected
+	if (((m_vInputPins[0] == NULL || m_vInputPins[0]->IsConnected() == FALSE) &&
+		(m_vInputPins[1] == NULL || m_vInputPins[1]->IsConnected() == FALSE))||
+		m_vOutputPins[0]->IsConnected() == FALSE) 
+	{
+		m_State = State_Stopped;
+		return NOERROR;
+	}
+
+	ASSERT(m_vOutputPins[0]);
+
+	// decommit the input pin before locking or we can deadlock
+	m_vInputPins[0]->Inactive();
+	m_vInputPins[1]->Inactive();
+
+	// synchronize with Receive calls
+	CAutoLock lck2(&m_csReceive);
+	m_vOutputPins[0]->Inactive();
+
+	// allow a class derived from CTransformFilter
+	// to know about starting and stopping streaming
+	// complete the state transition
+	m_State = State_Stopped;
+	return S_OK;
+}
+
+STDMETHODIMP VideoMixingBase::Pause()
+{
+	CAutoLock lck(&m_csFilter);
+	HRESULT hr = NOERROR;
+
+	if (m_State == State_Paused) {
+		// (This space left deliberately blank)
+	}
+
+	// If we have no input pin or it isn't yet connected then when we are
+	// asked to pause we deliver an end of stream to the downstream filter.
+	// This makes sure that it doesn't sit there forever waiting for
+	// samples which we cannot ever deliver without an input connection.
+
+	else if (m_vInputPins[0] == NULL || m_vInputPins[0]->IsConnected() == FALSE) 
+	{
+		if (m_vInputPins[1] == NULL || m_vInputPins[1]->IsConnected() == FALSE)
+		{
+			if (m_vOutputPins[0]) 
+			{
+				m_vOutputPins[0]->DeliverEndOfStream();
+				//m_bEOSDelivered = TRUE;
+			}
+			m_State = State_Paused;
+		}
+	}
+
+	// We may have an input connection but no output connection
+	// However, if we have an input pin we do have an output pin
+	else if (m_vOutputPins[0]->IsConnected() == FALSE) {
+		// TOREVISE: This step is taken from the CBaseFilter but doesn't prevent the graph from running
+		// m_State = State_Paused;
+		// However this line prevents the graph from running if the output pin of the multiplexer is not connected
+		return E_FAIL;
+
+	}
+	else 
+	{
+		if (m_State == State_Stopped) {
+			// allow a class derived from CTransformFilter
+			// to know about starting and stopping streaming
+			//CAutoLock lck2(&m_csReceive);
+			//hr = StartStreaming();
+		}
+		if (SUCCEEDED(hr)) {
+			hr = CBaseFilter::Pause();
+		}
+	}
+	return hr;
+}
---
+++ trunk/videoprocessing/Source/RtvcLib/DirectShow/VideoMixingBase.h
@@ -0,0 +1,116 @@
+/** @file
+
+MODULE                : VideoMixingBase
+
+FILE NAME            : VideoMixingBase.h
+
+DESCRIPTION            : Video mixing base class for RGB24 and RGB32 media
+                     
+LICENSE: Software License Agreement (BSD License)
+
+Copyright (c) 2008, Meraka Institute
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+* Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+* Neither the name of the Meraka Institute nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+===========================================================================
+*/
+#pragma once
+
+#include <DirectShow/MultiIOBaseFilter.h>
+
+class VideoMixingBase : public CMultiIOBaseFilter
+{
+public:
+	/// Destructor
+	~VideoMixingBase();
+
+	/// Subclass must override these methods: it is up to the implementer whether the samples should be copied
+	/// to a temporary buffer etc.
+	/// The user must also decide on how the output of the filter is regulated.
+	virtual HRESULT ReceiveFirstSample(IMediaSample *pSample) = 0;
+	virtual HRESULT ReceiveSecondSample(IMediaSample *pSample) = 0;
+
+	/// This method allows the subclass to create the appropriate video mixer based on the input media types
+	/// Implementations that do not require this functionality should return S_OK;
+	virtual HRESULT CreateVideoMixer(const CMediaType *pMediaType, int nIndex) = 0;
+
+	/// This method is responsible for setting the values of 
+	/// m_nOutputWidth;
+	/// m_nOutputHeight;
+	/// m_nOutputSize;
+	/// The values set in this method will be used in GetMediaType and hence in DecideBufferSize
+	/// The Subclass should also reject invalid media types in this method: e.g. a simple concatenation
+	/// filter should return E_FAIL from this method if the image dimensions do not match
+	virtual HRESULT SetOutputDimensions(BITMAPINFOHEADER* pBmih1, BITMAPINFOHEADER* pBmih2) = 0;
+
+	virtual HRESULT Receive(IMediaSample *pSample, int nIndex );
+
+
+	/// From CBaseOutputPin
+	virtual HRESULT DecideBufferSize(IMemAllocator* pAlloc, ALLOCATOR_PROPERTIES* pRequestProperties, int m_nIndex);
+
+	/// To Check media type
+	virtual HRESULT GetMediaType(int iPosition, CMediaType* pmt, int nOutputPinIndex) ;
+
+	/// Overriding this so that we can set whether this is an RGB24 or an RGB32 Filter and create
+	/// the picture concatentor dynamically. This method also tells us if the advert stream is tagged
+	HRESULT SetMediaType(PIN_DIRECTION direction, const CMediaType *pmt, int nIndex);
+
+	STDMETHODIMP Stop();
+	STDMETHODIMP Pause();
+
+	virtual void InitialiseInputTypes()
+	{
+		AddInputType(&MEDIATYPE_Video, &MEDIASUBTYPE_RGB24, &FORMAT_VideoInfo);
+		AddInputType(&MEDIATYPE_Video, &MEDIASUBTYPE_RGB32, &FORMAT_VideoInfo);
+	}
+	virtual void InitialiseOutputTypes()
+	{
+		AddOutputType(&MEDIATYPE_Video, &MEDIASUBTYPE_RGB24, &FORMAT_VideoInfo);
+		AddOutputType(&MEDIATYPE_Video, &MEDIASUBTYPE_RGB32, &FORMAT_VideoInfo);
+	}
+
+	virtual int InitialNumberOfInputPins()	{ return 2; }
+	virtual int InitialNumberOfOutputPins() { return 1; }
+	virtual bool OnFullCreateMoreInputs()	{ return false; }
+	virtual bool OnFullCreateMoreOutputs()	{ return false; }
+
+protected:
+	/// Constructor
+	VideoMixingBase(TCHAR *pObjectName, LPUNKNOWN lpUnk, CLSID clsid);
+
+	/// Stores video info headers
+	VIDEOINFOHEADER m_VideoInHeader[2];
+	
+	int m_nOutputWidth;
+	int m_nOutputHeight;
+	int m_nOutputSize;
+
+
+	/// Start time of the sample
+	REFERENCE_TIME m_tStartTime1;
+	REFERENCE_TIME m_tStartTime2;
+	/// End time of the sample
+	REFERENCE_TIME m_tStopTime1;
+	REFERENCE_TIME m_tStopTime2;	
+
+	/// Stores bytes per needed to store pixel according to media type
+	double m_nBytesPerPixel;
+};
