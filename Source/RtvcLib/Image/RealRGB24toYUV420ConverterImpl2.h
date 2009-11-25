36404f8a22399980a0b3153489894d3b1f0dd3c9
rtvc <rtvc@6f5d9dc4-7504-4b2d-819e-181cc167b626>
2009-11-25 10:08:26 +0000

    New implementations of RGB24 - YUV420 conversions with a bug fix when _BUILD_FOR_SHORT is off and a more flexible chr value offset.

---
+++ Source/RtvcLib/Image/RealRGB24toYUV420ConverterImpl2.h
@@ -0,0 +1,75 @@
+/** @file
+
+MODULE				: RealRGB24toYUV420ConverterImpl2
+
+TAG						: RRGB24YUVCI2
+
+FILE NAME			: RealRGB24toYUV420ConverterImpl2.h
+
+DESCRIPTION		: Double precision floating point RGB 24 bit to YUV420 colour 
+								convertion derived from the RGBtoYUV420Converter base class.
+								Use this implementation as the reference.
+
+COPYRIGHT			:	(c)CSIR 2007-2010 all rights resevered
+
+LICENSE				: Software License Agreement (BSD License)
+
+RESTRICTIONS	: Redistribution and use in source and binary forms, with or without 
+								modification, are permitted provided that the following conditions 
+								are met:
+
+								* Redistributions of source code must retain the above copyright notice, 
+								this list of conditions and the following disclaimer.
+								* Redistributions in binary form must reproduce the above copyright notice, 
+								this list of conditions and the following disclaimer in the documentation 
+								and/or other materials provided with the distribution.
+								* Neither the name of the CSIR nor the names of its contributors may be used 
+								to endorse or promote products derived from this software without specific 
+								prior written permission.
+
+								THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+								"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+								LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+								A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+								CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+								EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+								PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+								PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+								LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+								NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+								SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+===========================================================================
+*/
+#ifndef _REALRGB24TOYUV420CONVERTERIMPL2_H
+#define _REALRGB24TOYUV420CONVERTERIMPL2_H
+
+#include "DllSetup.h"
+
+#include "RGBtoYUV420Converter.h"
+
+/*
+===========================================================================
+  Class definition.
+===========================================================================
+*/
+class IMAGE_API RealRGB24toYUV420ConverterImpl2: public RGBtoYUV420Converter
+{
+public:
+	/// Construction and destruction.
+	RealRGB24toYUV420ConverterImpl2(void) { _chrOff = 0; }
+	RealRGB24toYUV420ConverterImpl2(int width, int height): RGBtoYUV420Converter(width,height) { _chrOff = 0; }
+	RealRGB24toYUV420ConverterImpl2(int width, int height, int chrOff): RGBtoYUV420Converter(width,height) { _chrOff = chrOff; }
+	virtual ~RealRGB24toYUV420ConverterImpl2(void) {}
+
+	/// Interface.
+	void Convert(void* pRgb, void* pY, void* pU, void* pV);
+
+protected:
+	/// Offset added to the chr values. Typically = 128 to shift all values to positive.
+	yuvType _chrOff;
+
+};//end _REALRGB24TOYUV420CONVERTERIMPL2_H.
+
+
+#endif
