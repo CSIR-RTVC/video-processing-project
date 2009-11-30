ad9211b7ed09ea5fec8bb4a35799eea1fa9d9d33
rglobisch <rglobisch@6f5d9dc4-7504-4b2d-819e-181cc167b626>
2009-11-30 22:45:15 +0000

    Updated property page

--- trunk/videoprocessing/Source/Filters/DirectShow/PicInPicFilter/PicInPicProperties.h
+++ trunk/videoprocessing/Source/Filters/DirectShow/PicInPicFilter/PicInPicProperties.h
@@ -46,92 +46,22 @@ class PicInPicProperties : public FilterPropertiesBase
 {
 public:
 
-	static CUnknown * WINAPI CreateInstance(LPUNKNOWN pUnk, HRESULT *pHr) 
-	{
-		PicInPicProperties *pNewObject = new PicInPicProperties(pUnk);
-		if (pNewObject == NULL) 
-		{
-			*pHr = E_OUTOFMEMORY;
-		}
-		return pNewObject;
-	}
+	static CUnknown * WINAPI CreateInstance(LPUNKNOWN pUnk, HRESULT *pHr);
 
-	PicInPicProperties::PicInPicProperties(IUnknown *pUnk) : 
-	FilterPropertiesBase(NAME("Pic in pic Properties"), pUnk, IDD_PIC_IN_PIC_DIALOG, IDS_PIC_IN_PIC_CAPTION)
-	{;}
+	PicInPicProperties(IUnknown *pUnk);
 
-	HRESULT ReadSettings()
-	{
-		initialiseControls();
-		return S_OK;
-		//int nLength = 0;
-		//char szBuffer[BUFFER_SIZE];
-		//HRESULT hr = m_pSettingsInterface->GetParameter(ORIENTATION, sizeof(szBuffer), szBuffer, &nLength);
-		//if (SUCCEEDED(hr))
-		//{
-		//	int nMixingMode = atoi(szBuffer);
-		//	int nRadioID = RADIO_BUTTON_IDS[nMixingMode];
-		//	long lResult = SendMessage(				// returns LRESULT in lResult
-		//		GetDlgItem(m_Dlg, nRadioID),		// handle to destination control
-		//		(UINT) BM_SETCHECK,					// message ID
-		//		(WPARAM) 1,							// = 0; not used, must be zero
-		//		(LPARAM) 0							// = (LPARAM) MAKELONG ((short) nUpper, (short) nLower)
-		//		);
-		//	return S_OK;
-		//}
-		//else
-		//{
-		//	return E_FAIL;
-		//}
-		//return hr;
-	}
+	HRESULT ReadSettings();
 
-	HRESULT OnApplyChanges(void)
-	{
-		//for (int i = 0; i < 2; ++i)
-		//{
-		//	int nRadioID = RADIO_BUTTON_IDS[i];
-		//	int iCheck = SendMessage( GetDlgItem(m_Dlg, nRadioID),	(UINT) BM_GETCHECK,	0, 0);
-		//	if (iCheck != 0)
-		//	{
-		//		std::string sID = StringUtil::itos(i);
-		//		HRESULT hr = m_pSettingsInterface->SetParameter(ORIENTATION, sID.c_str());
-		//		break;
-		//	}
-		//}
-		return S_OK;
-	}
+	HRESULT OnApplyChanges(void);
 
 private:
-	void initialiseControls()
-	{
-		InitCommonControls();
+	void initialiseControls();
 
-		SendMessage(GetDlgItem(m_Dlg, IDC_CMB_POSITION), CB_RESETCONTENT, 0, 0);
-		//Add default option
-		SendMessage(GetDlgItem(m_Dlg, IDC_CMB_POSITION), CB_ADDSTRING,	   0, (LPARAM)"Left bottom");
-		SendMessage(GetDlgItem(m_Dlg, IDC_CMB_POSITION), CB_SELECTSTRING,  0, (LPARAM)"Left bottom");
-		// Now populate the graphs
-		SendMessage(GetDlgItem(m_Dlg, IDC_CMB_POSITION), CB_INSERTSTRING,  1, (LPARAM)"Left top");
-		SendMessage(GetDlgItem(m_Dlg, IDC_CMB_POSITION), CB_INSERTSTRING,  2, (LPARAM)"Right top");
-		SendMessage(GetDlgItem(m_Dlg, IDC_CMB_POSITION), CB_INSERTSTRING,  3, (LPARAM)"Right bottom");
-		SendMessage(GetDlgItem(m_Dlg, IDC_CMB_POSITION), CB_INSERTSTRING,  4, (LPARAM)"Custom");
-		SendMessage(GetDlgItem(m_Dlg, IDC_CMB_POSITION), CB_SETMINVISIBLE, 5, 0);
-
-		//// Init frames per second
-		//short lower = 1;
-		//short upper = 60;
-
-		//// Init UI
-		//long lResult = SendMessage(			// returns LRESULT in lResult
-		//	GetDlgItem(m_Dlg, IDC_SPIN_FPS),	// handle to destination control
-		//	(UINT) UDM_SETRANGE,			// message ID
-		//	(WPARAM) 0,						// = 0; not used, must be zero
-		//	(LPARAM) MAKELONG ( upper, lower)      // = (LPARAM) MAKELONG ((short) nUpper, (short) nLower)
-		//	);
-		//// Default value of 30 FPS
-		//SetDlgItemText(m_Dlg, IDC_FPS, "30");
-
-	}
+	unsigned m_targetWidth;
+	unsigned m_targetHeight;
+	unsigned m_uiSubpicWidth;
+	unsigned m_uiSubpicHeight;
+	unsigned m_uiOffsetX;
+	unsigned m_uiOffsetY;
 };
 
