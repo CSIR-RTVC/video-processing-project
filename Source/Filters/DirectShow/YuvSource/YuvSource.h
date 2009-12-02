e8f2035369bdfd8d862d37a0f066642601eb929b
rglobisch <rglobisch@6f5d9dc4-7504-4b2d-819e-181cc167b626>
2009-12-02 22:38:59 +0000

    Added include

--- Source/Filters/DirectShow/YuvSource/YuvSource.h
+++ Source/Filters/DirectShow/YuvSource/YuvSource.h
@@ -33,6 +33,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 #include <strsafe.h>
 
+#include <fstream>
 #include <DirectShow/CStatusInterface.h>
 #include <DirectShow/CSettingsInterface.h>
 
