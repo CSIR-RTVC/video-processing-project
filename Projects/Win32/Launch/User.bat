b828aedf79adee77848f1c121242a73050c2b449
rglobisch <rglobisch@6f5d9dc4-7504-4b2d-819e-181cc167b626>
2010-10-08 06:21:00 +0000

    Removed manual root directory

--- Projects/Win32/Launch/User.bat
+++ Projects/Win32/Launch/User.bat
@@ -1,11 +1,7 @@
 @echo Running User.bat commands
 
 @echo Setting DirectShow and SDK Paths
-@set DSHOWBASECLASSES=C:\Program Files\Microsoft SDKs\Windows\v6.0\Samples\Multimedia\DirectShow\BaseClasses
+@set DSHOWBASECLASSES=C:\Program Files\Microsoft SDKs\Windows\v7.0\Samples\Multimedia\DirectShow\BaseClasses
 @echo -- DSHOWBASECLASSES set to %DSHOWBASECLASSES%
-@set WindowsSDKDir=C:\Program Files\Microsoft SDKs\Windows\v6.0
+@set WindowsSDKDir=C:\Program Files\Microsoft SDKs\Windows\v7.0
 @echo -- WindowsSDKDir set to %WindowsSDKDir%
-
-@echo Setting Video Processing Root
-@set RTVCRootDir=<<videoprocessingroot>>
-@echo -- RTVCRootDir set to %RTVCRootDir%
