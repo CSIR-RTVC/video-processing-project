20ab6059ecd4f987fd9ec860a21d847cf55e390e
rglobisch <rglobisch@6f5d9dc4-7504-4b2d-819e-181cc167b626>
2010-10-08 06:21:49 +0000

    Added comment for 64/bit OS

--- Projects/Win32/Launch/VsVersion.bat
+++ Projects/Win32/Launch/VsVersion.bat
@@ -7,6 +7,7 @@ IF %VS_VERSION% EQU VC8 (
 @set VisualStudioRoot="C:\Program Files\Microsoft Visual Studio 8"
 )
 
+REM On a 64 bit OS -> set this to something like "C:\Program Files (x86)\Microsoft Visual Studio 9.0"
 IF %VS_VERSION% EQU VC9 (
 @set VisualStudioRoot="C:\Program Files\Microsoft Visual Studio 9.0"
 )
