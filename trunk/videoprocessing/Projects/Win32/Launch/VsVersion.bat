49ad808054fd3ee4f38637a4dfc6c36cb012576d
rglobisch <rglobisch@6f5d9dc4-7504-4b2d-819e-181cc167b626>
2010-11-09 09:02:51 +0000

    Added VC10 support

--- trunk/videoprocessing/Projects/Win32/Launch/VsVersion.bat
+++ trunk/videoprocessing/Projects/Win32/Launch/VsVersion.bat
@@ -1,16 +1,30 @@
-@echo Running VsVersion.bat commands
-
-REM Set this to VC8 or VC9 depending on which VS version you want to use
+REM Set this to VC8, VC9 or VC10 depending on which VS version you want to use
 @set VS_VERSION=VC9
+REM On a 64 bit OS -> set the path to something like "C:\Program Files (x86)\Microsoft Visual Studio 9.0"
 
-IF %VS_VERSION% EQU VC8 (
-@set VisualStudioRoot="C:\Program Files\Microsoft Visual Studio 8"
-)
+IF %VS_VERSION% EQU VC8 GOTO SETUP_VC8_ENV
+IF %VS_VERSION% EQU VC9 GOTO SETUP_VC9_ENV
+IF %VS_VERSION% EQU VC10 GOTO SETUP_VC10_ENV
 
-REM On a 64 bit OS -> set this to something like "C:\Program Files (x86)\Microsoft Visual Studio 9.0"
-IF %VS_VERSION% EQU VC9 (
-@set VisualStudioRoot="C:\Program Files\Microsoft Visual Studio 9.0"
-)
+:SETUP_VC8_ENV
+@set VisualStudioRoot=C:\Program Files\Microsoft Visual Studio 8
+@set VisualStudio=%VisualStudioRoot%\Common7\IDE\devenv.exe
+@set VisualCDir=VC
+@GOTO END
 
+:SETUP_VC9_ENV
+@set VisualStudioRoot=C:\Program Files\Microsoft Visual Studio 9.0
+@set VisualStudio=%VisualStudioRoot%\Common7\IDE\devenv.exe
+@set VisualCDir=VC
+@GOTO END
+
+:SETUP_VC10_ENV
+@set VisualStudioRoot=C:\Program Files (x86)\Microsoft Visual Studio 10.0
+@set VisualStudio=%VisualStudioRoot%\Common7\IDE\devenv.exe
+@set VisualCDir=VC
+@GOTO END
+
+:END
 @echo -- VisualStudioRoot set to %VisualStudioRoot%
+@echo -- VisualStudio set to %VisualStudio%
 @echo -- VS_SOL_EXT set to %VS_SOL_EXT%
