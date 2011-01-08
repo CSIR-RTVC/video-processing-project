141ecb29b4fcb29ced4f72801483379624e74e5f
rglobisch <rglobisch@6f5d9dc4-7504-4b2d-819e-181cc167b626>
2011-01-08 17:03:45 +0000

    Added TimerUtil and update typedefs

--- Source/RtvcLib/Shared/CommonDefs.h
+++ Source/RtvcLib/Shared/CommonDefs.h
@@ -1,4 +1,11 @@
 #pragma once
 
+#include <map>
+#include <string>
+#include <vector>
+
+typedef std::vector<std::string> StringList_t;
+typedef std::map<std::string, std::string> StringMap_t;
+
 #define SAFE_DELETE(a) if(a){delete a;a=NULL;}
 
