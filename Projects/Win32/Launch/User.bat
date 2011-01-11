56b60ab4c039c6313594e0b3f88e936e6562bf2e
rglobisch <rglobisch@6f5d9dc4-7504-4b2d-819e-181cc167b626>
2011-01-11 05:36:27 +0000

    Updated batch files for VC express

--- Projects/Win32/Launch/User.bat
+++ Projects/Win32/Launch/User.bat
@@ -1,7 +1,7 @@
 @echo Running User.bat commands
 
 @echo Setting DirectShow and SDK Paths
-@set DSHOWBASECLASSES=C:\Program Files\Microsoft SDKs\Windows\v7.0\Samples\Multimedia\DirectShow\BaseClasses
+@set DSHOWBASECLASSES=D:\Program Files\Microsoft SDKs\Windows\v7.0\Samples\multimedia\directshow\baseclasses
 @echo -- DSHOWBASECLASSES set to %DSHOWBASECLASSES%
-@set WindowsSDKDir=C:\Program Files\Microsoft SDKs\Windows\v7.0
+@set WindowsSDKDir=D:\Program Files\Microsoft SDKs\Windows\v7.0
 @echo -- WindowsSDKDir set to %WindowsSDKDir%
