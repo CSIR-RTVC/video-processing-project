db5def78e89ef87ba519d21ddcf147e58d05246d
rglobisch <rglobisch@6f5d9dc4-7504-4b2d-819e-181cc167b626>
2011-01-11 05:36:27 +0000

    Updated batch files for VC express

--- trunk/videoprocessing/Projects/Win32/Launch/VsVersion.bat
+++ trunk/videoprocessing/Projects/Win32/Launch/VsVersion.bat
@@ -1,6 +1,9 @@
 REM Set this to VC8, VC9 or VC10 depending on which VS version you want to use
-@set VS_VERSION=VC9
+@set VS_VERSION=VC10
 REM On a 64 bit OS -> set the path to something like "C:\Program Files (x86)\Microsoft Visual Studio 9.0"
+
+REM Set this to devenv.exe or VCExpress.exe
+@set VC_EXE=VCExpress.exe
 
 IF %VS_VERSION% EQU VC8 GOTO SETUP_VC8_ENV
 IF %VS_VERSION% EQU VC9 GOTO SETUP_VC9_ENV
@@ -8,19 +11,19 @@ IF %VS_VERSION% EQU VC10 GOTO SETUP_VC10_ENV
 
 :SETUP_VC8_ENV
 @set VisualStudioRoot=C:\Program Files\Microsoft Visual Studio 8
-@set VisualStudio=%VisualStudioRoot%\Common7\IDE\devenv.exe
+@set VisualStudio=%VisualStudioRoot%\Common7\IDE\%VC_EXE%
 @set VisualCDir=VC
 @GOTO END
 
 :SETUP_VC9_ENV
 @set VisualStudioRoot=C:\Program Files\Microsoft Visual Studio 9.0
-@set VisualStudio=%VisualStudioRoot%\Common7\IDE\devenv.exe
+@set VisualStudio=%VisualStudioRoot%\Common7\IDE\%VC_EXE%
 @set VisualCDir=VC
 @GOTO END
 
 :SETUP_VC10_ENV
-@set VisualStudioRoot=C:\Program Files (x86)\Microsoft Visual Studio 10.0
-@set VisualStudio=%VisualStudioRoot%\Common7\IDE\devenv.exe
+@set VisualStudioRoot=D:\Program Files\Microsoft Visual Studio 10.0
+@set VisualStudio=%VisualStudioRoot%\Common7\IDE\%VC_EXE%
 @set VisualCDir=VC
 @GOTO END
 
