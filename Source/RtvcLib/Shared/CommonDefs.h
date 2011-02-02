a2540ff4ddf43944ed64d7b64f2665ae10ac3414
rglobisch <rglobisch@6f5d9dc4-7504-4b2d-819e-181cc167b626>
2011-02-02 07:41:54 +0000

    Added SafeDeleteArray

--- Source/RtvcLib/Shared/CommonDefs.h
+++ Source/RtvcLib/Shared/CommonDefs.h
@@ -9,3 +9,14 @@ typedef std::map<std::string, std::string> StringMap_t;
 
 #define SAFE_DELETE(a) if(a){delete a;a=NULL;}
 
+template< class T > void SafeDelete( T*& pVal )
+{
+  delete pVal;
+  pVal = NULL;
+}
+
+template< class T > void SafeDeleteArray( T*& pVal )
+{
+  delete[] pVal;
+  pVal = NULL;
+}
