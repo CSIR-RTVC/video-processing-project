3ee6fda941fcb141a05d6e3c8909238455de354e
rglobisch <rglobisch@6f5d9dc4-7504-4b2d-819e-181cc167b626>
2011-02-02 07:42:54 +0000

    Updated for code sprint demo

---
+++ trunk/videoprocessing/Source/RtvcLib/Image/FastGpuRGB24toYUV420Converter.h
@@ -0,0 +1,58 @@
+/** @file
+
+MODULE				: FastGpuRGB24toYUV420Converter
+
+FILE NAME			: FastGpuRGB24toYUV420Converter.h
+
+DESCRIPTION			: GPU-based RGB 24 bit to YUV420 colour 
+					conversion derived from the RGBtoYUV420Converter base class.
+					Use this implementation as the reference.
+	  
+LICENSE: Software License Agreement (BSD License)
+
+Copyright (c) 2008, CSIR
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+* Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+* Neither the name of the CSIR nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+===========================================================================
+*/
+#pragma once
+
+#include "RGBtoYUV420Converter.h"
+
+/**
+ * \ingroup ImageLib
+ * GPU-based RGB 24 bit to YUV420 colour 
+ * convertion derived from the RGBtoYUV420Converter base class.
+ * Use this implementation as the reference.
+ */
+class FastGpuRGB24toYUV420Converter: public RGBtoYUV420Converter
+{
+public:
+    /// Construction and destruction.
+    FastGpuRGB24toYUV420Converter(void) {}
+    FastGpuRGB24toYUV420Converter(int width, int height): RGBtoYUV420Converter(width,height) {}
+    FastGpuRGB24toYUV420Converter(int width, int height, int chrOff): RGBtoYUV420Converter(width,height, chrOff) {}
+    virtual ~FastGpuRGB24toYUV420Converter(void) {}
+
+    /// Interface.
+    void Convert(void* pRgb, void* pY, void* pU, void* pV);
+
+};
