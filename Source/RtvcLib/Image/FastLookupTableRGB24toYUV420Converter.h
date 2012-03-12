d07f3d2292defb3529f2818480d02c84bbd7a64a
rglobisch <rglobisch@6f5d9dc4-7504-4b2d-819e-181cc167b626>
2012-03-12 05:45:44 +0000

    Updated copyright notice

--- Source/RtvcLib/Image/FastLookupTableRGB24toYUV420Converter.h
+++ Source/RtvcLib/Image/FastLookupTableRGB24toYUV420Converter.h
@@ -10,7 +10,7 @@ DESCRIPTION			: Lookup-table-based  RGB 24 bit to YUV420 colour
 	  
 LICENSE: Software License Agreement (BSD License)
 
-Copyright (c) 2008, CSIR
+Copyright (c) 2008 - 2012, CSIR
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
