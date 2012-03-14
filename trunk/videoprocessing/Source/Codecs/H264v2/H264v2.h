520533a408b268fdb43108a18ecce360c882f816
rglobisch <rglobisch@6f5d9dc4-7504-4b2d-819e-181cc167b626>
2012-03-14 13:14:06 +0000

    Updated license (LGPL)

--- trunk/videoprocessing/Source/Codecs/H264v2/H264v2.h
+++ trunk/videoprocessing/Source/Codecs/H264v2/H264v2.h
@@ -1,3 +1,34 @@
+/** @file
+
+MODULE				: H264
+
+TAG					: H264
+
+FILE NAME			: H264v2.h
+
+DESCRIPTION			: H264v2Factory header	
+
+LICENSE	: GNU Lesser General Public License
+
+Copyright (c) 2008 - 2012, CSIR
+All rights reserved.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+===========================================================================
+*/
+
 // The following ifdef block is the standard way of creating macros which make exporting 
 // from a DLL simpler. All files within this DLL are compiled with the H264V2_EXPORTS
 // symbol defined on the command line. this symbol should not be defined on any project
--- trunk/videoprocessing/Source/Codecs/H264v2/stdafx.cpp
+++ trunk/videoprocessing/Source/Codecs/H264v2/stdafx.cpp
@@ -1,5 +1,31 @@
-// stdafx.cpp : source file that includes just the standard includes
-// H264v2.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
+/** @file
 
+MODULE				: H264
+
+TAG					: H264
+
+FILE NAME			: stdafx.cpp
+
+DESCRIPTION		:	
+
+LICENSE	: GNU Lesser General Public License
+
+Copyright (c) 2008 - 2012, CSIR
+All rights reserved.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+===========================================================================
+*/
 #include "stdafx.h"
