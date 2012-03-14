1a1710b3643c3726165e1cc38e9836b06bbc8fa5
rglobisch <rglobisch@6f5d9dc4-7504-4b2d-819e-181cc167b626>
2012-03-14 12:47:30 +0000

    Updated license (LGPL)

--- trunk/videoprocessing/Source/Codecs/CodecUtils/BitStreamReader.h
+++ trunk/videoprocessing/Source/Codecs/CodecUtils/BitStreamReader.h
@@ -16,11 +16,24 @@ DESCRIPTION		: A bit stream reader implementation of the BitStreamBase
 									.
 									delete pBsr;
 
-REVISION HISTORY	:
+LICENSE	: GNU Lesser General Public License
 
-COPYRIGHT					:
+Copyright (c) 2008 - 2012, CSIR
+All rights reserved.
 
-RESTRICTIONS			: 
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ===========================================================================
 */
 #ifndef _BITSTREAMREADER_H
