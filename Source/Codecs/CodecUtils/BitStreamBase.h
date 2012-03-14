79a10b087d9a97fc8c52db529da26d53f9d42766
rglobisch <rglobisch@6f5d9dc4-7504-4b2d-819e-181cc167b626>
2012-03-14 12:47:30 +0000

    Updated license (LGPL)

--- Source/Codecs/CodecUtils/BitStreamBase.h
+++ Source/Codecs/CodecUtils/BitStreamBase.h
@@ -11,11 +11,25 @@ DESCRIPTION				: A base class to contain a contiguous mem array to be
 										Derived classes handle the reading and writing, 
 										BitStreamReader and BitStreamWriter.
 
-REVISION HISTORY	:
+LICENSE	: GNU Lesser General Public License
 
-COPYRIGHT					:
+Copyright (c) 2008 - 2012, CSIR
+All rights reserved.
 
-RESTRICTIONS			: 
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 ===========================================================================
 */
 #ifndef _BITSTREAMBASE_H
