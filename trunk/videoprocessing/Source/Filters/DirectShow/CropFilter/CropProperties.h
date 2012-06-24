ea0a1764cbee78f6a0e244465193aacce45e7a2f
rglobisch <rglobisch@6f5d9dc4-7504-4b2d-819e-181cc167b626>
2012-06-24 10:03:03 +0000

    Refactored

--- trunk/videoprocessing/Source/Filters/DirectShow/CropFilter/CropProperties.h
+++ trunk/videoprocessing/Source/Filters/DirectShow/CropFilter/CropProperties.h
@@ -32,7 +32,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ===========================================================================
 */
 #pragma once
-
+#include <cassert>
 #include <DirectShow/FilterPropertiesBase.h>
 
 #include "resource.h"
@@ -63,105 +63,45 @@ public:
 
 	HRESULT ReadSettings()
 	{
-		short lower = 0;
-		short upper = SHRT_MAX;
+    setSpinBoxRange(IDC_SPIN1, 0, SHRT_MAX);
+    setSpinBoxRange(IDC_SPIN2, 0, SHRT_MAX);
+    setSpinBoxRange(IDC_SPIN3, 0, SHRT_MAX);
+    setSpinBoxRange(IDC_SPIN4, 0, SHRT_MAX);
 
-		// Init UI
-		long lResult = SendMessage(			// returns LRESULT in lResult
-			GetDlgItem(m_Dlg, IDC_SPIN1),	// handle to destination control
-			(UINT) UDM_SETRANGE,			// message ID
-			(WPARAM) 0,						// = 0; not used, must be zero
-			(LPARAM) MAKELONG ( upper, lower)      // = (LPARAM) MAKELONG ((short) nUpper, (short) nLower)
-			);
-		lResult = SendMessage(			// returns LRESULT in lResult
-			GetDlgItem(m_Dlg, IDC_SPIN2),	// handle to destination control
-			(UINT) UDM_SETRANGE,			// message ID
-			(WPARAM) 0,						// = 0; not used, must be zero
-			(LPARAM) MAKELONG ( upper, lower)      // = (LPARAM) MAKELONG ((short) nUpper, (short) nLower)
-			);
-		lResult = SendMessage(			// returns LRESULT in lResult
-			GetDlgItem(m_Dlg, IDC_SPIN3),	// handle to destination control
-			(UINT) UDM_SETRANGE,			// message ID
-			(WPARAM) 0,						// = 0; not used, must be zero
-			(LPARAM) MAKELONG ( upper, lower)      // = (LPARAM) MAKELONG ((short) nUpper, (short) nLower)
-			);
-		lResult = SendMessage(			// returns LRESULT in lResult
-			GetDlgItem(m_Dlg, IDC_SPIN4),	// handle to destination control
-			(UINT) UDM_SETRANGE,			// message ID
-			(WPARAM) 0,						// = 0; not used, must be zero
-			(LPARAM) MAKELONG ( upper, lower)      // = (LPARAM) MAKELONG ((short) nUpper, (short) nLower)
-			);
+    // top
+    HRESULT hr = setEditTextFromIntFilterParameter(TOP_CROP, IDC_EDIT_TOP);
+    if (FAILED(hr)) return hr;
+    // bottom
+    hr = setEditTextFromIntFilterParameter(BOTTOM_CROP, IDC_EDIT_BOTTOM);
+    if (FAILED(hr)) return hr;
+    // left
+    hr = setEditTextFromIntFilterParameter(LEFT_CROP, IDC_EDIT_LEFT);
+    if (FAILED(hr)) return hr;
+    // right
+    hr = setEditTextFromIntFilterParameter(RIGHT_CROP, IDC_EDIT_RIGHT);
+    if (FAILED(hr)) return hr;
 
-
-		int nLength = 0;
-		char szBuffer[BUFFER_SIZE];
-		HRESULT hr = m_pSettingsInterface->GetParameter(TOP_CROP, sizeof(szBuffer), szBuffer, &nLength);
-		if (SUCCEEDED(hr))
-		{
-			SetDlgItemText(m_Dlg, IDC_EDIT_TOP, szBuffer);
-			m_topCrop = atoi(szBuffer);
-		}
-		else
-		{
-			return E_FAIL;
-		}
-		hr = m_pSettingsInterface->GetParameter(BOTTOM_CROP, sizeof(szBuffer), szBuffer, &nLength);
-		if (SUCCEEDED(hr))
-		{
-			SetDlgItemText(m_Dlg, IDC_EDIT_BOTTOM, szBuffer);
-			m_bottomCrop = atoi(szBuffer);
-		}
-		else
-		{
-			return E_FAIL;
-		}
-		hr = m_pSettingsInterface->GetParameter(LEFT_CROP, sizeof(szBuffer), szBuffer, &nLength);
-		if (SUCCEEDED(hr))
-		{
-			SetDlgItemText(m_Dlg, IDC_EDIT_LEFT, szBuffer);
-			m_leftCrop = atoi(szBuffer);
-		}
-		else
-		{
-			return E_FAIL;
-		}
-		hr = m_pSettingsInterface->GetParameter(RIGHT_CROP, sizeof(szBuffer), szBuffer, &nLength);
-		if (SUCCEEDED(hr))
-		{
-			SetDlgItemText(m_Dlg, IDC_EDIT_RIGHT, szBuffer);
-			m_rightCrop = atoi(szBuffer);
-		}
-		else
-		{
-			return E_FAIL;
-		}
-		return hr;
+    return hr;
 	}
 
 	HRESULT OnApplyChanges(void)
 	{
-		int nLength = 0;
-		char szBuffer[BUFFER_SIZE];
-		nLength = GetDlgItemText(m_Dlg, IDC_EDIT_TOP, szBuffer, BUFFER_SIZE);
-		m_topCrop = atoi(szBuffer);
-		m_pSettingsInterface->SetParameter(TOP_CROP, szBuffer);
-		nLength = GetDlgItemText(m_Dlg, IDC_EDIT_BOTTOM, szBuffer, BUFFER_SIZE);
-		m_bottomCrop = atoi(szBuffer);
-		m_pSettingsInterface->SetParameter(BOTTOM_CROP, szBuffer);
-		nLength = GetDlgItemText(m_Dlg, IDC_EDIT_LEFT, szBuffer, BUFFER_SIZE);
-		m_leftCrop = atoi(szBuffer);
-		m_pSettingsInterface->SetParameter(LEFT_CROP, szBuffer);
-		nLength = GetDlgItemText(m_Dlg, IDC_EDIT_RIGHT, szBuffer, BUFFER_SIZE);
-		m_rightCrop = atoi(szBuffer);
-		m_pSettingsInterface->SetParameter(RIGHT_CROP, szBuffer);
+    // top
+    HRESULT hr = setIntFilterParameterFromEditText(TOP_CROP, IDC_EDIT_TOP);
+    assert(SUCCEEDED(hr));
+    // bottom
+    hr = setIntFilterParameterFromEditText(BOTTOM_CROP, IDC_EDIT_BOTTOM);
+    assert(SUCCEEDED(hr));
+    // left
+    hr = setIntFilterParameterFromEditText(LEFT_CROP, IDC_EDIT_LEFT);
+    assert(SUCCEEDED(hr));
+    // right
+    hr = setIntFilterParameterFromEditText(RIGHT_CROP, IDC_EDIT_RIGHT);
+    assert(SUCCEEDED(hr));
 		return S_OK;
 	} 
 
 private:
 
-	unsigned m_topCrop;
-	unsigned m_bottomCrop;
-	unsigned m_leftCrop;
-	unsigned m_rightCrop;
 };
 
