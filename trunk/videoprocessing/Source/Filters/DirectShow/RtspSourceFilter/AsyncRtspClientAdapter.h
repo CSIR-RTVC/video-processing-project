8492b95e3bb52ca6d56776206f90c10484259926
rglobisch <rglobisch@6f5d9dc4-7504-4b2d-819e-181cc167b626>
2014-08-18 11:40:39 +0000

    Classes for async live555 update

---
+++ trunk/videoprocessing/Source/Filters/DirectShow/RtspSourceFilter/AsyncRtspClientAdapter.h
@@ -0,0 +1,128 @@
+/** @file
+
+MODULE				: RtspSourceFilter
+
+FILE NAME			: AsyncRtspClientAdapter.h
+
+DESCRIPTION			: Adapter class for asynchronous callbacks
+					  
+LICENSE: Software License Agreement (BSD License)
+
+Copyright (c) 2014, CSIR
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+* Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+* Neither the name of the CSIR nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+===========================================================================
+*/
+#pragma once
+#include <boost/function.hpp>
+
+class AsyncRtspClientAdapter : public RTSPClient
+{
+public:
+  typedef boost::function<void(int resultCode, char* resultString)> RtspCallback_t;
+
+public:
+  static AsyncRtspClientAdapter* createNew(UsageEnvironment& env, char const* rtspURL,
+    int verbosityLevel = 0,
+    char const* applicationName = NULL,
+    portNumBits tunnelOverHTTPPortNum = 0);
+
+  AsyncRtspClientAdapter(UsageEnvironment& env, char const* rtspURL, int verbosityLevel, char const* applicationName, portNumBits tunnelOverHTTPPortNum);
+
+  // Callbacks
+  void setOptionsCallback(AsyncRtspClientAdapter::RtspCallback_t val) { m_optionsCallback = val; }
+  void setDescribeCallback(AsyncRtspClientAdapter::RtspCallback_t val) { m_describeCallback = val; }
+  void setSetupCallback(AsyncRtspClientAdapter::RtspCallback_t val) { m_setupCallback = val; }
+  void setPlayCallback(AsyncRtspClientAdapter::RtspCallback_t val) { m_playCallback = val; }
+  void setTeardownCallback(AsyncRtspClientAdapter::RtspCallback_t val) { m_teardownCallback = val; }
+
+  // Need a static method to get a regular function pointer
+  static void optionsHandler(RTSPClient* _this, int resultCode, char* resultString) 
+  {
+    AsyncRtspClientAdapter* pClient = static_cast<AsyncRtspClientAdapter*>(_this);
+    pClient->doHandleOptions(resultCode, resultString);
+  }
+
+  void doHandleOptions(int resultCode, char* resultString) 
+  {
+    if (m_optionsCallback)
+      m_optionsCallback(resultCode, resultString);
+  }
+
+  // Need a static method to get a regular function pointer
+  static void describeHandler(RTSPClient* _this, int resultCode, char* resultString) 
+  {
+    AsyncRtspClientAdapter* pClient = static_cast<AsyncRtspClientAdapter*>(_this);
+    pClient->doHandleDescribe(resultCode, resultString);
+  }
+
+  void doHandleDescribe(int resultCode, char* resultString) 
+  {
+    if (m_describeCallback)
+      m_describeCallback(resultCode, resultString);
+  }
+
+  // Need a static method to get a regular function pointer
+  static void setupHandler(RTSPClient* _this, int resultCode, char* resultString) 
+  {
+    AsyncRtspClientAdapter* pClient = static_cast<AsyncRtspClientAdapter*>(_this);
+    pClient->doHandleSetup(resultCode, resultString);
+  }
+
+  void doHandleSetup(int resultCode, char* resultString) 
+  {
+    if (m_setupCallback)
+      m_setupCallback(resultCode, resultString);
+  }
+
+  // Need a static method to get a regular function pointer
+  static void playHandler(RTSPClient* _this, int resultCode, char* resultString) 
+  {
+    AsyncRtspClientAdapter* pClient = static_cast<AsyncRtspClientAdapter*>(_this);
+    pClient->doHandlePlay(resultCode, resultString);
+  }
+
+  void doHandlePlay(int resultCode, char* resultString) 
+  {
+    if (m_playCallback)
+      m_playCallback(resultCode, resultString);
+  }
+
+  // Need a static method to get a regular function pointer
+  static void teardownHandler(RTSPClient* _this, int resultCode, char* resultString) 
+  {
+    AsyncRtspClientAdapter* pClient = static_cast<AsyncRtspClientAdapter*>(_this);
+    pClient->doHandleTeardown(resultCode, resultString);
+  }
+
+  void doHandleTeardown(int resultCode, char* resultString) 
+  {
+    if (m_teardownCallback)
+      m_teardownCallback(resultCode, resultString);
+  }
+
+private:
+  RtspCallback_t m_optionsCallback;
+  RtspCallback_t m_describeCallback;
+  RtspCallback_t m_setupCallback;
+  RtspCallback_t m_playCallback;
+  RtspCallback_t m_teardownCallback;
+};
---
+++ trunk/videoprocessing/Source/Filters/DirectShow/RtspSourceFilter/AsyncRtspClientSessionManager.h
@@ -0,0 +1,187 @@
+/** @file
+
+MODULE				: RtspSourceFilter
+
+FILE NAME			: AsyncRtspClientSessionManager.h
+
+DESCRIPTION			: RTSP client session manager - encapsulates liveMedia functionality
+					  
+LICENSE: Software License Agreement (BSD License)
+
+Copyright (c) 2014, CSIR
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+* Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+* Neither the name of the CSIR nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+===========================================================================
+*/
+#pragma once
+#include "AsyncRtspClientAdapter.h"
+#include "MediaPacketManager.h"
+
+// Forward
+class TaskScheduler;
+class UsageEnvironment;
+class RTSPClient;
+class MediaSession;
+class MediaSubsession;
+class RtvcRtpSink;
+
+class AsyncRtspClientSessionManager
+{
+public:
+
+  AsyncRtspClientSessionManager(MediaPacketManager& rMediaPacketManager);
+  ~AsyncRtspClientSessionManager();
+
+  std::string getLastError() const { return m_sLastError; }
+  int getLastLiveMediaErrorCode() const { return m_iLastLiveMediaErrorCode; }
+
+  bool getStreamUsingTcp() const { return m_bStreamUsingTcp; }
+  void setStreamUsingTcp(bool val) { m_bStreamUsingTcp = val; }
+
+  bool getStreamOverHttp() const { return m_bStreamOverHttp; }
+  void setStreamOverHttp(bool val) { m_bStreamOverHttp = val; }
+
+  MediaPacketManager& getMediaPacketManager() const { return m_rMediaPacketManager; }
+  MediaSession* getMediaSession() const { return m_pSession; }
+
+  bool setupSuccessful() const
+  {
+    return m_eState == RTSP_PLAYING;
+  }
+
+  void startRtspSession(const std::string& sUrl);
+  void endLiveMediaEventLoop();
+
+  void subsessionAfterPlaying(RtvcRtpSink* pSink);
+
+private:
+  // Prohibit copying
+  AsyncRtspClientSessionManager(const RtspClientSessionManager&);
+  AsyncRtspClientSessionManager operator=(const RtspClientSessionManager&);
+
+  void createMediaSession( const std::string& sUrl );
+  void setupMediaSession();
+  void setupMediaSubsession(MediaSubsession* pSubsession);
+
+  void setupReceivers();
+
+  void teardownMediaSession();
+  void endSession();
+
+  void cleanupLiveMedia()
+  {
+    // Cleanup RTSP Media session
+    teardownMediaSession();
+
+    if (m_pScheduler)
+      delete m_pScheduler;
+    m_pScheduler = NULL;
+
+    if (m_pEnv)
+      m_pEnv->reclaim(); // Deletes itself
+    m_pEnv = NULL;
+
+    m_uiSubsessionCount = 0;
+    m_uiSuccessfulSubsessions = 0;
+
+    // memory is managed by livemedia
+    m_pSubsession = NULL;
+
+    if (m_pSubsessionIter)
+    {
+      delete m_pSubsessionIter;
+      m_pSubsessionIter = NULL;
+    }
+
+    resetWatchVariable();
+  }
+
+  /// Bye Handler
+  static void subsessionByeHandler(void* clientData) 
+  {
+    AsyncRtspClientSessionManager* pRtspSessionManager = (AsyncRtspClientSessionManager*) clientData;
+    pRtspSessionManager->endLiveMediaEventLoop();
+  }
+
+  void sessionAfterPlaying();
+private:
+
+  bool createRtpSource(MediaSubsession* pSubsession);
+  void closeMediaSinks();
+  void tearDownStreams();
+
+  /// Call this method to end the life media event loop
+  void setWatchVariable() { m_watchVariable = 1; }
+  void resetWatchVariable() { m_watchVariable = 0; }
+  bool createReceivers(MediaSession* pSession);
+
+private:
+
+  // Forward function definitions:
+  void continueAfterOPTIONS(int resultCode, char* resultString);
+  void continueAfterDESCRIBE(int resultCode, char* resultString);
+  void continueAfterSETUP(int resultCode, char* resultString);
+  void continueAfterPLAY(int resultCode, char* resultString);
+  void continueAfterTEARDOWN(int resultCode, char* resultString);
+
+private:
+
+  enum RtspState
+  {
+    RTSP_INIT,    // state until RTSP SETUP is successful
+    RTSP_SETUP,   // state until RTSP PLAY is successful
+    RTSP_PLAYING
+  };
+  RtspState m_eState;
+
+  // Stores last error
+  std::string m_sLastError;
+
+  MediaPacketManager& m_rMediaPacketManager;
+  /// TCP
+  bool m_bStreamUsingTcp;
+  /// HTTP tunneling
+  bool m_bStreamOverHttp;
+
+  /// Livemedia scheduler
+  TaskScheduler* m_pScheduler;
+  /// Usage environment
+  UsageEnvironment* m_pEnv;
+
+  AsyncRtspClientAdapter* m_pRtspClient;
+
+  std::string m_sUrl;
+  /// MediaSession
+  MediaSession* m_pSession;
+  /// MediaSubsession
+  MediaSubsession* m_pSubsession;
+  /// count of media subsessions used for async setup
+  unsigned m_uiSubsessionCount;
+  unsigned m_uiSuccessfulSubsessions;
+  /// iterator used for async setup
+  MediaSubsessionIterator* m_pSubsessionIter;
+
+  /// LiveMediaEventLoop watch variable
+  char m_watchVariable;
+  /// Live media error code
+  int m_iLastLiveMediaErrorCode;
+};
+
