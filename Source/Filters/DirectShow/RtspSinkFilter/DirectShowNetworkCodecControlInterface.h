7afc3c09c8226f4bf63e502cf074f5933e4974d9
rglobisch <rglobisch@6f5d9dc4-7504-4b2d-819e-181cc167b626>
2014-09-05 12:11:17 +0000

    Implemented DirectShowNetworkCodecControlInterface

--- Source/Filters/DirectShow/RtspSinkFilter/DirectShowNetworkCodecControlInterface.h
+++ Source/Filters/DirectShow/RtspSinkFilter/DirectShowNetworkCodecControlInterface.h
@@ -1,20 +1,71 @@
 #pragma once
 #include "INetworkCodecControlInterface.h"
 
+// DirectShow includes
+#pragma warning(push)     // disable for this header only
+#pragma warning(disable:4312) 
+// DirectShow
+#include <Streams.h>
+#pragma warning(pop)      // restore original warning level
+
+#include <DirectShow/ComTypeDefs.h>
+#include <DirectShow/DirectShowHelper.h>
+
 class DirectShowNetworkCodecControlInterface : public INetworkCodecControlInterface
 {
 public:
 
+  DirectShowNetworkCodecControlInterface(IBaseFilterPtr pThisFilter)
+    :m_pThisFilter(pThisFilter),
+    m_pCodecInterface(NULL),
+    m_hrInterfaceAquired(S_OK)
+  {
+
+  }
+  
   virtual void setFramebitLimit(unsigned uiFrameBitLimit)
   {
-    // TODO:
-
+    VLOG(5) << "setFramebitLimit called: " << uiFrameBitLimit;
+    // only try once to get the interface
+    if (FAILED(m_hrInterfaceAquired)) return;
+    if (!m_pCodecInterface)
+    {
+      m_hrInterfaceAquired = CDirectShowHelper::FindFirstInterface(m_pThisFilter, PINDIR_INPUT, IID_ICodecControlInterface, (void **)&m_pCodecInterface);
+      if (FAILED(m_hrInterfaceAquired))
+      {
+        LOG(WARNING) << "Failed to find CodecControlinterface ";
+        return;
+      }
+    }
+    HRESULT hr = m_pCodecInterface->SetFramebitLimit(uiFrameBitLimit);
+    if (FAILED(hr))
+    {
+      LOG(WARNING) << "Failed to set frame bit limit to " << uiFrameBitLimit;
+    }
   }
 
   virtual void generateIdr()
   {
-    // TODO:
-
+    VLOG(5) << "generateIdr called";
+    if (FAILED(m_hrInterfaceAquired)) return;
+    if (!m_pCodecInterface)
+    {
+      m_hrInterfaceAquired = CDirectShowHelper::FindFirstInterface(m_pThisFilter, PINDIR_INPUT, IID_ICodecControlInterface, (void **)&m_pCodecInterface);
+      if (FAILED(m_hrInterfaceAquired))
+      {
+        LOG(WARNING) << "Failed to find CodecControlinterface ";
+        return;
+      }
+    }
+    HRESULT hr = m_pCodecInterface->GenerateIdr();
+    if (FAILED(hr))
+    {
+      LOG(WARNING) << "Failed to set generate IDR";
+    }
   }
 
+private:
+  IBaseFilterPtr m_pThisFilter;
+  ICodecControlInterfacePtr m_pCodecInterface;
+  HRESULT m_hrInterfaceAquired;
 };
