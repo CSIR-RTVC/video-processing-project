af88f789820ef6599699f4a78320a5b40d4bd1de
rglobisch <rglobisch@6f5d9dc4-7504-4b2d-819e-181cc167b626>
2014-09-05 18:43:01 +0000

    Removed using namespacec std. Formatting

--- Source/RtvcLib/DirectShow/CustomBaseFilter.h
+++ Source/RtvcLib/DirectShow/CustomBaseFilter.h
@@ -5,10 +5,10 @@ MODULE				: CustomBaseFilter
 FILE NAME			: CustomBaseFilter.h
 
 DESCRIPTION			: Base class for CSIR DirectShow filters with one input and one output pin.
-					  
+
 LICENSE: Software License Agreement (BSD License)
 
-Copyright (c) 2008 - 2012, CSIR
+Copyright (c) 2008 - 2014, CSIR
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
@@ -37,8 +37,6 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 typedef std::vector<GUID*> GUIDList_t;
 
-using namespace std;
-
 /**
  * \defgroup DirectShowFilters DirectShow Filters
  * \defgroup DirectShowLib DirectShow Library
@@ -51,81 +49,75 @@ using namespace std;
  */
 class CCustomBaseFilter : public CTransformFilter, public CSettingsInterface, public CStatusInterface
 {
-	///this needs to be declared for the extra interface (adds the COM AddRef, etc methods)
-	DECLARE_IUNKNOWN;
+  /////this needs to be declared for the extra interface (adds the COM AddRef, etc methods)
+  //DECLARE_IUNKNOWN;
 
 public:
-	/// Constructor
-	CCustomBaseFilter(TCHAR *pObjectName, LPUNKNOWN lpUnk, CLSID clsid);
-	/// Destructor
-	virtual ~CCustomBaseFilter(void);
-	
-	/// Overridden method which validates the input media type against the ones accepted by this filter
-	virtual HRESULT CheckInputType(const CMediaType *pMediaType);
+  /// Constructor
+  CCustomBaseFilter(TCHAR *pObjectName, LPUNKNOWN lpUnk, CLSID clsid);
+  /// Destructor
+  virtual ~CCustomBaseFilter(void);
 
-	/// Helper method which allows us to capture the input output media type properties
-	/// Override this method to set other initialization code but call this base class method first
-	virtual HRESULT SetMediaType(PIN_DIRECTION direction, const CMediaType *pmt);
+  /// Overridden method which validates the input media type against the ones accepted by this filter
+  virtual HRESULT CheckInputType(const CMediaType *pMediaType);
 
-	/// Transform code
-	/**
-	* This method takes the source sample and transforms it and writes the transformed sample to the destination
-	* This method is called by the transform input pin's Receive method.
-	* @param pSource The source sample
-	* @param pDest The destination sample
-	*/
-	HRESULT Transform(IMediaSample *pSource, IMediaSample *pDest);
+  /// Helper method which allows us to capture the input output media type properties
+  /// Override this method to set other initialization code but call this base class method first
+  virtual HRESULT SetMediaType(PIN_DIRECTION direction, const CMediaType *pmt);
 
-	/// override this to publicize our interfaces
-	STDMETHODIMP NonDelegatingQueryInterface(REFIID riid, void **ppv);
-	
-	// TOREMOVE
-	/// Overridden from CSettingsInterface
-	// STDMETHODIMP GetParameter( const char* szParamName, char* szResult, int& length );
-	/// Overridden from CSettingsInterface
-	// STDMETHODIMP SetParameter( const char* szParamName, const char* szValue);
+  /// Transform code
+  /**
+   * @brief This method takes the source sample and transforms it and writes the transformed sample to the destination
+   * This method is called by the transform input pin's Receive method.
+   * @param pSource The source sample
+   * @param pDest The destination sample
+   */
+  HRESULT Transform(IMediaSample *pSource, IMediaSample *pDest);
 
-	/// This method needs to overridden by child classes - add all possible accepted input combinations
-	/// using the AddInputType method
-	virtual void InitialiseInputTypes() PURE;
+  /// override this to publicize our interfaces
+  STDMETHODIMP NonDelegatingQueryInterface(REFIID riid, void **ppv);
+
+  /// This method needs to overridden by child classes - add all possible accepted input combinations
+  /// using the AddInputType method
+  virtual void InitialiseInputTypes() PURE;
 
 protected:
-	/**
-	* Used for Media Type Negotiation 
-	* Call the AddInputType method in your overridden InitialiseInputTypes 
-	* method to add your acceptable media types, subtypes and formats.
-	*/	
-	void AddInputType(const GUID* pType, const GUID* pSubType, const GUID* pFormat);
+  /**
+   * Used for Media Type Negotiation
+   * Call the AddInputType method in your overridden InitialiseInputTypes
+   * method to add your acceptable media types, subtypes and formats.
+   */
+  void AddInputType(const GUID* pType, const GUID* pSubType, const GUID* pFormat);
 
-	/**
-	* This method must be overridden in the sub class to apply the filter specific transformation
-	* It must return the size of the resulting transformed output buffer
-	*/
+  /**
+   * This method must be overridden in the sub class to apply the filter specific transformation
+   * It must return the size of the resulting transformed output buffer
+   */
   virtual void ApplyTransform(BYTE* pBufferIn, long lInBufferSize, long lActualDataLength, BYTE* pBufferOut, long lOutBufferSize, long& lOutActualDataLength) PURE;
 
-	/// Video header of input
-	VIDEOINFOHEADER m_videoInHeader;
-	/// Video header of output
-	VIDEOINFOHEADER m_videoOutHeader;
-	// Other video information
-	/// Height of image
-	int m_nInHeight;
-	/// Width of image
-	int m_nInWidth;
-	/// Number of pixels in image
-	int m_nInPixels;
-	/// Height in output image
-	int m_nOutHeight;
-	/// Width in output image
-	int m_nOutWidth;
-	/// Number of pixels in output image
-	int m_nOutPixels;
-	/// Bit count of image
-	int m_nBitCount;
+  /// Video header of input
+  VIDEOINFOHEADER m_videoInHeader;
+  /// Video header of output
+  VIDEOINFOHEADER m_videoOutHeader;
+  // Other video information
+  /// Height of image
+  int m_nInHeight;
+  /// Width of image
+  int m_nInWidth;
+  /// Number of pixels in image
+  int m_nInPixels;
+  /// Height in output image
+  int m_nOutHeight;
+  /// Width in output image
+  int m_nOutWidth;
+  /// Number of pixels in output image
+  int m_nOutPixels;
+  /// Bit count of image
+  int m_nBitCount;
 
 private:
-	// Used for storing and checking connection types
-	GUIDList_t m_vInTypes;
-	GUIDList_t m_vInSubTypes;
-	GUIDList_t m_vInFormats;
+  // Used for storing and checking connection types
+  GUIDList_t m_vInTypes;
+  GUIDList_t m_vInSubTypes;
+  GUIDList_t m_vInFormats;
 };
