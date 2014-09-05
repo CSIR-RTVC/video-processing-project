7ef645d3385290b0ee2411e6155f24f90e34aa4f
rglobisch <rglobisch@6f5d9dc4-7504-4b2d-819e-181cc167b626>
2014-09-05 12:10:04 +0000

    Added ICodecControlInterface and ComTypeDefs.h

---
+++ trunk/videoprocessing/Source/RtvcLib/DirectShow/CodecControlInterface.h
@@ -0,0 +1,58 @@
+/** @file
+
+MODULE				: CodecControlInterface
+
+FILE NAME			: CodecControlInterface.h
+
+DESCRIPTION			: COM settings interface to control the codec.
+Supports using smart COM pointers
+
+LICENSE: Software License Agreement (BSD License)
+
+Copyright (c) 2008 - 2014, CSIR
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+* Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+* Neither the name of the CSIR nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+===========================================================================
+*/
+#pragma once
+
+//This file needs to be included - else we get a linker error for the GUID
+#include <INITGUID.H>
+
+/*! \interface ISettingsInterface
+\brief COM Settings interface for VPP DirectShow filters.
+*/
+
+//For Smart pointers
+
+// {A33BCB92-5EF6-4A66-9988-5EEF2B8C8944}
+DEFINE_GUID(IID_ICodecControlInterface ,
+  0xa33bcb92, 0x5ef6, 0x4a66, 0x99, 0x88, 0x5e, 0xef, 0x2b, 0x8c, 0x89, 0x44);
+
+#undef  INTERFACE
+#define INTERFACE   ICodecControlInterface
+DECLARE_INTERFACE_(ICodecControlInterface, IUnknown)
+{
+  /// Method to set frame bit limit
+  STDMETHOD(SetFramebitLimit)(unsigned uiFrameBitLimit) = 0;
+  /// Method to trigger IDR frame
+  STDMETHOD(GenerateIdr)() = 0;
+};
---
+++ trunk/videoprocessing/Source/RtvcLib/DirectShow/ComTypeDefs.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#pragma warning(push)     // disable for this header only
+#pragma warning(disable:4312)
+// DirectShow
+#include <streams.h>
+#pragma warning(pop)      // restore original warning level
+
+#include <comdef.h>
+
+#include <DirectShow/SettingsInterface.h>
+#include <DirectShow/StatusInterface.h>
+#include <DirectShow/CodecControlInterface.h>
+
+// DirectShow COM smart pointers
+_COM_SMARTPTR_TYPEDEF(IGraphBuilder, IID_IGraphBuilder);
+_COM_SMARTPTR_TYPEDEF(IMediaControl, IID_IMediaControl);
+_COM_SMARTPTR_TYPEDEF(IMediaEvent, IID_IMediaEvent);
+_COM_SMARTPTR_TYPEDEF(IMediaEventEx, IID_IMediaEventEx);
+_COM_SMARTPTR_TYPEDEF(IMediaEventSink, IID_IMediaEventSink);
+_COM_SMARTPTR_TYPEDEF(IMediaSeeking, IID_IMediaSeeking);
+_COM_SMARTPTR_TYPEDEF(IBaseFilter, IID_IBaseFilter);
+_COM_SMARTPTR_TYPEDEF(ICaptureGraphBuilder2, IID_ICaptureGraphBuilder2);
+_COM_SMARTPTR_TYPEDEF(ICreateDevEnum, IID_ICreateDevEnum);
+_COM_SMARTPTR_TYPEDEF(IPin, IID_IPin);
+_COM_SMARTPTR_TYPEDEF(IFileSourceFilter, IID_IFileSourceFilter);
+_COM_SMARTPTR_TYPEDEF(IFileSinkFilter, IID_IFileSinkFilter);
+_COM_SMARTPTR_TYPEDEF(IAMAnalogVideoDecoder, IID_IAMAnalogVideoDecoder);
+
+//Sample Grabber Interface
+#include <QEdit.h>
+_COM_SMARTPTR_TYPEDEF(ISampleGrabber, IID_ISampleGrabber);
+_COM_SMARTPTR_TYPEDEF(ISampleGrabberCB, IID_ISampleGrabberCB);
+// For compression
+_COM_SMARTPTR_TYPEDEF(IAMStreamConfig, IID_IAMStreamConfig);
+// Audio buffer latency configuration
+_COM_SMARTPTR_TYPEDEF(IAMBufferNegotiation, IID_IAMBufferNegotiation);
+// IReferenceClock
+_COM_SMARTPTR_TYPEDEF(IReferenceClock, IID_IReferenceClock);
+_COM_SMARTPTR_TYPEDEF(IMediaFilter, IID_IMediaFilter);
+_COM_SMARTPTR_TYPEDEF(IAMCrossbar, IID_IAMCrossbar);
+
+//Smart pointers for RTVC COM interfaces
+_COM_SMARTPTR_TYPEDEF(ISettingsInterface, IID_ISettingsInterface);
+_COM_SMARTPTR_TYPEDEF(IStatusInterface, IID_IStatusInterface);
+_COM_SMARTPTR_TYPEDEF(ICodecControlInterface, IID_ICodecControlInterface);
