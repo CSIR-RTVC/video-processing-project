f78bebd86ca3ff101783d6bc03b2bf7181215be5
rglobisch <rglobisch@6f5d9dc4-7504-4b2d-819e-181cc167b626>
2014-11-06 18:14:27 +0000

    Formatting

--- trunk/videoprocessing/Source/Filters/DirectShow/ScaleFilter/ScaleFilter.h
+++ trunk/videoprocessing/Source/Filters/DirectShow/ScaleFilter/ScaleFilter.h
@@ -33,7 +33,6 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ===========================================================================
 */
 #pragma once
-
 #include <DirectShow/CustomBaseFilter.h>
 #include <Filters/DirectShow/FilterParameters.h>
 
