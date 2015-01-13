a64188a5fdee370f7eff186d66a67a0f3d4ab433
rglobisch <rglobisch@6f5d9dc4-7504-4b2d-819e-181cc167b626>
2015-01-13 09:44:47 +0000

    Code cleanup: renamed CCropFilter to CropFilter. Formatting.

--- trunk/videoprocessing/Source/Filters/DirectShow/CropFilter/CropFilter.h
+++ trunk/videoprocessing/Source/Filters/DirectShow/CropFilter/CropFilter.h
@@ -33,7 +33,6 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ===========================================================================
 */
 #pragma once
-
 // CSIR includes
 #include <DirectShow/CustomBaseFilter.h>
 #include <Filters/DirectShow/FilterParameters.h>
@@ -50,51 +49,49 @@ static const GUID CLSID_CropProperties =
 class PicCropperBase;
 
 /**
-*  Crop filter for RGB24 and RGB32 media. Crop parameters can be set using the COM SettingsInterface SetParameter method.
-*/
-class CCropFilter : public CCustomBaseFilter,
+ * @brief Crop filter for RGB24 and RGB32 media. 
+ * 
+ * Crop parameters can be set using the COM SettingsInterface SetParameter method.
+ */
+class CropFilter : public CCustomBaseFilter,
   public ISpecifyPropertyPages
 {
 public:
   DECLARE_IUNKNOWN
 
   /// Constructor
-  CCropFilter();
+  CropFilter();
   /// Destructor
-  ~CCropFilter();
-
-  /// Static object-creation method (for the class factory)
+  ~CropFilter();
+  /**
+   * @brief Static object-creation method (for the class factory)
+   */
   static CUnknown * WINAPI CreateInstance(LPUNKNOWN pUnk, HRESULT *pHr);
 
   /**
-  * Overriding this so that we can set whether this is an RGB24 or an RGB32 Filter
-  */
+   * Overriding this so that we can set whether this is an RGB24 or an RGB32 Filter
+   */
   HRESULT SetMediaType(PIN_DIRECTION direction, const CMediaType *pmt);
-
   /**
-  * Used for Media Type Negotiation
-  * Returns an HRESULT value. Possible values include those shown in the following table.
-  * <table border="0" cols="2"><tr valign="top"><td><b>Value</b></td><td><b>Description</b></td></TR><TR><TD>S_OK</TD><TD>Success</TD></TR><TR><TD>VFW_S_NO_MORE_ITEMS</TD><TD>Index out of range</TD></TR><TR><TD>E_INVALIDARG</TD><TD>Index less than zero</TD></TR></TABLE>
-  * The output pin's CTransformOutputPin::GetMediaType method calls this method. The derived class must implement this method. For more information, see CBasePin::GetMediaType.
-  * To use custom media types, the media type can be manipulated in this method.
-  */
+   * @brief Used for Media Type Negotiation
+   * Returns an HRESULT value. Possible values include those shown in the following table.
+   * <table border="0" cols="2"><tr valign="top"><td><b>Value</b></td><td><b>Description</b></td></TR><TR><TD>S_OK</TD><TD>Success</TD></TR><TR><TD>VFW_S_NO_MORE_ITEMS</TD><TD>Index out of range</TD></TR><TR><TD>E_INVALIDARG</TD><TD>Index less than zero</TD></TR></TABLE>
+   * The output pin's CTransformOutputPin::GetMediaType method calls this method. The derived class must implement this method. For more information, see CBasePin::GetMediaType.
+   * To use custom media types, the media type can be manipulated in this method.
+   */
   HRESULT GetMediaType(int iPosition, CMediaType *pMediaType);
-
-  /// Buffer Allocation
   /**
-  * The output pin's CTransformOutputPin::DecideBufferSize method calls this method. The derived class must implement this method. For more information, see CBaseOutputPin::DecideBufferSize.
-  * @param pAlloc Pointer to the IMemAllocator interface on the output pin's allocator.
-  * @param pProp Pointer to an ALLOCATOR_PROPERTIES structure that contains buffer requirements from the downstream input pin.
-  * @return Value: Returns S_OK or another HRESULT value.
-  */
+   * @brief The output pin's CTransformOutputPin::DecideBufferSize method calls this method. The derived class must implement this method. For more information, see CBaseOutputPin::DecideBufferSize.
+   * @param pAlloc Pointer to the IMemAllocator interface on the output pin's allocator.
+   * @param pProp Pointer to an ALLOCATOR_PROPERTIES structure that contains buffer requirements from the downstream input pin.
+   * @return Value: Returns S_OK or another HRESULT value.
+   */
   HRESULT DecideBufferSize(IMemAllocator *pAlloc, ALLOCATOR_PROPERTIES *pProp);
-
   /**
-  * The CheckTransform method checks whether an input media type is compatible with an output media type.
-  * <table border="0" cols="2"> <tr valign="top"> <td  width="50%"><b>Value</b></td> <td width="50%"><b>Description</b></td> </tr> <tr valign="top"> <td width="50%">S_OK</td> <td width="50%">The media types are compatible.</td> </tr> <tr valign="top"> <td width="50%">VFW_E_TYPE_NOT_ACCEPTED</td> <td width="50%">The media types are not compatible.</td> </tr> </table>
-  */
+   * @brief The CheckTransform method checks whether an input media type is compatible with an output media type.
+   * <table border="0" cols="2"> <tr valign="top"> <td  width="50%"><b>Value</b></td> <td width="50%"><b>Description</b></td> </tr> <tr valign="top"> <td width="50%">S_OK</td> <td width="50%">The media types are compatible.</td> </tr> <tr valign="top"> <td width="50%">VFW_E_TYPE_NOT_ACCEPTED</td> <td width="50%">The media types are not compatible.</td> </tr> </table>
+   */
   HRESULT CheckTransform(const CMediaType *mtIn, const CMediaType *mtOut);
-
   /// Overridden from CSettingsInterface
   virtual void initParameters()
   {
@@ -106,12 +103,11 @@ public:
     addParameter(BOTTOM_CROP, &m_nBottomCrop, 0);
     RecalculateFilterParameters();
   }
-
+  /// Overridden from CSettingsInterface
   STDMETHODIMP SetParameter(const char* type, const char* value);
-
   /// Overridden from CCustomBaseFilter
   virtual void InitialiseInputTypes();
-
+  /// Overridden from ISpecifyPropertyPages
   STDMETHODIMP GetPages(CAUUID *pPages)
   {
     if (pPages == NULL) return E_POINTER;
@@ -124,7 +120,7 @@ public:
     pPages->pElems[0] = CLSID_CropProperties;
     return S_OK;
   }
-
+  /// Overridden from COM
   STDMETHODIMP NonDelegatingQueryInterface(REFIID riid, void **ppv)
   {
     if (riid == IID_ISpecifyPropertyPages)
@@ -140,17 +136,15 @@ public:
 
 private:
   /**
-  * This method converts the input buffer from RGB24 | 32 to YUV420P
-  * @param pSource The source buffer
-  * @param pDest The destination buffer
-  */
+   * This method applies the crop transform
+   * @param pSource The source buffer
+   * @param pDest The destination buffer
+   */
   virtual HRESULT ApplyTransform(BYTE* pBufferIn, long lInBufferSize, long lActualDataLength, BYTE* pBufferOut, long lOutBufferSize, long& lOutActualDataLength);
-
-  /// Sets crop if values are valid
+  /// Sets crop parameters if values are valid
   HRESULT SetCropIfValid(int nTotalDimensionImage, int nNewCrop, int& nOldCrop, int nOppositeCrop);
   /// This method calculates how much should be cropped from the picture based on the crop ratios
   void RecalculateFilterParameters();
-
   /// Pointer to our picture cropper
   PicCropperBase* m_pCropper;
 
@@ -160,7 +154,6 @@ private:
   int m_nRightCrop;
   int m_nTopCrop;
   int m_nBottomCrop;
-
   int m_nStride;
   int m_nPadding;
 };
