06234773d3e437c00b9db1671134307ba468cfb7
rglobisch <rglobisch@6f5d9dc4-7504-4b2d-819e-181cc167b626>
2015-01-14 09:41:09 +0000

    Formatting

--- Source/Filters/DirectShow/ScaleFilter/ScaleFilter.h
+++ Source/Filters/DirectShow/ScaleFilter/ScaleFilter.h
@@ -5,11 +5,11 @@ MODULE				: ScaleFilter
 FILE NAME			: ScaleFilter.h
 
 DESCRIPTION			: Scale Filter for RGB24 and YUV420P Media
-					Scale parameters are settable via the ISettingsInterface COM interface.
-					  
+Scale parameters are settable via the ISettingsInterface COM interface.
+
 LICENSE: Software License Agreement (BSD License)
 
-Copyright (c) 2008 - 2012, CSIR
+Copyright (c) 2008 - 2015, CSIR
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
@@ -37,18 +37,17 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <Filters/DirectShow/FilterParameters.h>
 
 // {60E0821D-9E6C-48f4-8E1C-1B25E67488F8}
-static const GUID CLSID_ScaleFilter = 
+static const GUID CLSID_ScaleFilter =
 { 0x60e0821d, 0x9e6c, 0x48f4, { 0x8e, 0x1c, 0x1b, 0x25, 0xe6, 0x74, 0x88, 0xf8 } };
 
 // {1DE541CE-7FF7-47be-9CE7-055E6C2BD53F}
-static const GUID CLSID_ScaleProperties = 
+static const GUID CLSID_ScaleProperties =
 { 0x1de541ce, 0x7ff7, 0x47be, { 0x9c, 0xe7, 0x5, 0x5e, 0x6c, 0x2b, 0xd5, 0x3f } };
-
 
 #ifdef _BUILD_FOR_SHORT
 typedef short yuvType;
 #else
-typedef signed char yuvType ;
+typedef signed char yuvType;
 #endif
 
 // Forward declarations
@@ -60,96 +59,86 @@ class PicScalerBase;
  * Scale parameters are settable via the ISettingsInterface COM interface.
  */
 class ScaleFilter : public CCustomBaseFilter,
-					public ISpecifyPropertyPages
+  public ISpecifyPropertyPages
 {
 public:
-	DECLARE_IUNKNOWN;
+  DECLARE_IUNKNOWN;
 
-	/// Constructor
-	ScaleFilter();
-	/// Destructor
-	~ScaleFilter();
-
-	/// Static object-creation method (for the class factory)
-	static CUnknown * WINAPI CreateInstance(LPUNKNOWN pUnk, HRESULT *pHr); 
-
-	/**
-	* Overriding this so that we can set whether this is an RGB24 or an RGB32 Filter
-	*/
-	HRESULT SetMediaType(PIN_DIRECTION direction, const CMediaType *pmt);
-
-	/**
-	* Used for Media Type Negotiation 
-	* Returns an HRESULT value. Possible values include those shown in the following table.
-	* <table border="0" cols="2"><tr valign="top"><td><b>Value</b></td><td><b>Description</b></td></TR><TR><TD>S_OK</TD><TD>Success</TD></TR><TR><TD>VFW_S_NO_MORE_ITEMS</TD><TD>Index out of range</TD></TR><TR><TD>E_INVALIDARG</TD><TD>Index less than zero</TD></TR></TABLE>
-	* The output pin's CTransformOutputPin::GetMediaType method calls this method. The derived class must implement this method. For more information, see CBasePin::GetMediaType.
-	* To use custom media types, the media type can be manipulated in this method.
-	*/
-	HRESULT GetMediaType(int iPosition, CMediaType *pMediaType);
-
-	/// Buffer Allocation
-	/**
-	* The output pin's CTransformOutputPin::DecideBufferSize method calls this method. The derived class must implement this method. For more information, see CBaseOutputPin::DecideBufferSize. 
-	* @param pAlloc Pointer to the IMemAllocator interface on the output pin's allocator.
-	* @param pProp Pointer to an ALLOCATOR_PROPERTIES structure that contains buffer requirements from the downstream input pin.
-	* @return Value: Returns S_OK or another HRESULT value.
-	*/
-	HRESULT DecideBufferSize(IMemAllocator *pAlloc, ALLOCATOR_PROPERTIES *pProp);
-
-	/**
-	* The CheckTransform method checks whether an input media type is compatible with an output media type.
-	* <table border="0" cols="2"> <tr valign="top"> <td  width="50%"><b>Value</b></td> <td width="50%"><b>Description</b></td> </tr> <tr valign="top"> <td width="50%">S_OK</td> <td width="50%">The media types are compatible.</td> </tr> <tr valign="top"> <td width="50%">VFW_E_TYPE_NOT_ACCEPTED</td> <td width="50%">The media types are not compatible.</td> </tr> </table>
-	*/
-	HRESULT CheckTransform(const CMediaType *mtIn, const CMediaType *mtOut);
-
-	/// Overridden from CCustomBaseFilter
-	virtual void InitialiseInputTypes();
-
-	///Overridden from CSettingsInterface
-	virtual void initParameters()
-	{
-		addParameter(TARGET_WIDTH, &m_nOutWidth, 0);
-		addParameter(TARGET_HEIGHT, &m_nOutHeight, 0);
-	}
-	STDMETHODIMP SetParameter( const char* type, const char* value);
-
-	STDMETHODIMP GetPages(CAUUID *pPages)
-	{
-		if (pPages == NULL) return E_POINTER;
-		pPages->cElems = 1;
-		pPages->pElems = (GUID*)CoTaskMemAlloc(sizeof(GUID));
-		if (pPages->pElems == NULL) 
-		{
-			return E_OUTOFMEMORY;
-		}
-		pPages->pElems[0] = CLSID_ScaleProperties;
-		return S_OK;
-	}
-
-	STDMETHODIMP NonDelegatingQueryInterface(REFIID riid, void **ppv)
-	{
-		if (riid == IID_ISpecifyPropertyPages)
-		{
-			return GetInterface(static_cast<ISpecifyPropertyPages*>(this), ppv);
-		}
-		else
-		{
-			// Call the parent class.
-			return CCustomBaseFilter::NonDelegatingQueryInterface(riid, ppv);
-		}
-	}
+  /// Constructor
+  ScaleFilter();
+  /// Destructor
+  ~ScaleFilter();
+  /// Static object-creation method (for the class factory)
+  static CUnknown * WINAPI CreateInstance(LPUNKNOWN pUnk, HRESULT *pHr); 
+  /**
+   * Overriding this so that we can set whether this is an RGB24 or an RGB32 Filter
+   */
+  HRESULT SetMediaType(PIN_DIRECTION direction, const CMediaType *pmt);
+  /**
+   * Used for Media Type Negotiation
+   * Returns an HRESULT value. Possible values include those shown in the following table.
+   * <table border="0" cols="2"><tr valign="top"><td><b>Value</b></td><td><b>Description</b></td></TR><TR><TD>S_OK</TD><TD>Success</TD></TR><TR><TD>VFW_S_NO_MORE_ITEMS</TD><TD>Index out of range</TD></TR><TR><TD>E_INVALIDARG</TD><TD>Index less than zero</TD></TR></TABLE>
+   * The output pin's CTransformOutputPin::GetMediaType method calls this method. The derived class must implement this method. For more information, see CBasePin::GetMediaType.
+   * To use custom media types, the media type can be manipulated in this method.
+   */
+  HRESULT GetMediaType(int iPosition, CMediaType *pMediaType);
+  /**
+   * The output pin's CTransformOutputPin::DecideBufferSize method calls this method. The derived class must implement this method. For more information, see CBaseOutputPin::DecideBufferSize.
+   * @param pAlloc Pointer to the IMemAllocator interface on the output pin's allocator.
+   * @param pProp Pointer to an ALLOCATOR_PROPERTIES structure that contains buffer requirements from the downstream input pin.
+   * @return Value: Returns S_OK or another HRESULT value.
+   */
+  HRESULT DecideBufferSize(IMemAllocator *pAlloc, ALLOCATOR_PROPERTIES *pProp);
+  /**
+   * The CheckTransform method checks whether an input media type is compatible with an output media type.
+   * <table border="0" cols="2"> <tr valign="top"> <td  width="50%"><b>Value</b></td> <td width="50%"><b>Description</b></td> </tr> <tr valign="top"> <td width="50%">S_OK</td> <td width="50%">The media types are compatible.</td> </tr> <tr valign="top"> <td width="50%">VFW_E_TYPE_NOT_ACCEPTED</td> <td width="50%">The media types are not compatible.</td> </tr> </table>
+   */
+  HRESULT CheckTransform(const CMediaType *mtIn, const CMediaType *mtOut);
+  /// Overridden from CCustomBaseFilter
+  virtual void InitialiseInputTypes();
+  ///Overridden from CSettingsInterface
+  virtual void initParameters()
+  {
+    addParameter(TARGET_WIDTH, &m_nOutWidth, 0);
+    addParameter(TARGET_HEIGHT, &m_nOutHeight, 0);
+  }
+  STDMETHODIMP SetParameter(const char* type, const char* value);
+  STDMETHODIMP GetPages(CAUUID *pPages)
+  {
+    if (pPages == NULL) return E_POINTER;
+    pPages->cElems = 1;
+    pPages->pElems = (GUID*)CoTaskMemAlloc(sizeof(GUID));
+    if (pPages->pElems == NULL)
+    {
+      return E_OUTOFMEMORY;
+    }
+    pPages->pElems[0] = CLSID_ScaleProperties;
+    return S_OK;
+  }
+  STDMETHODIMP NonDelegatingQueryInterface(REFIID riid, void **ppv)
+  {
+    if (riid == IID_ISpecifyPropertyPages)
+    {
+      return GetInterface(static_cast<ISpecifyPropertyPages*>(this), ppv);
+    }
+    else
+    {
+      // Call the parent class.
+      return CCustomBaseFilter::NonDelegatingQueryInterface(riid, ppv);
+    }
+  }
 private:
-	/**
-	* This method converts the input buffer from RGB24 | 32 to YUV420P
-	* @param pSource The source buffer
-	* @param pDest The destination buffer
-	*/
+  /**
+   * This method converts the input buffer from RGB24 | 32 to YUV420P
+   * @param pSource The source buffer
+   * @param pDest The destination buffer
+   */
   virtual HRESULT ApplyTransform(BYTE* pBufferIn, long lInBufferSize, long lActualDataLength, BYTE* pBufferOut, long lOutBufferSize, long& lOutActualDataLength);
-	
-	HRESULT RecalculateFilterParameters();
 
-	/// Pointer to our picture scaler
-	PicScalerBase* m_pScaler;
-	/// Stores bytes per needed to store pixel according to media type
-	double m_nBytesPerPixel;
+  HRESULT RecalculateFilterParameters();
+
+  /// Pointer to our picture scaler
+  PicScalerBase* m_pScaler;
+  /// Stores bytes per needed to store pixel according to media type
+  double m_nBytesPerPixel;
 };
--- Source/Filters/DirectShow/ScaleFilter/stdafx.h
+++ Source/Filters/DirectShow/ScaleFilter/stdafx.h
@@ -4,11 +4,11 @@ MODULE				: stdafx
 
 FILE NAME			: stdafx.h
 
-DESCRIPTION			: 
-					  
+DESCRIPTION			:
+
 LICENSE: Software License Agreement (BSD License)
 
-Copyright (c) 2008 - 2012, CSIR
+Copyright (c) 2008 - 2015, CSIR
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
